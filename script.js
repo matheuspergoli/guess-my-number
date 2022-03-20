7fe1361a142e14dbd108abf233e9ad8fe6b65b04
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2022-03-19 22:21:22 -0300

    Adicionando cor
    Mudando a cor do background quando o player ganhar

--- script.js
+++ script.js
@@ -16,10 +16,18 @@ let score = 20
 document.querySelector('.check').addEventListener('click', () => {
     const guess = Number(document.querySelector('.guess').value)
 
+    // When there is no input
     if (!guess) {
         message('⛔ No number!')
+
+      // When player wins  
     } else if (guess === secretNumber) {
         message('👏 Correct Number!')
+
+        document.querySelector('body').style.backgroundColor = '#60b347'
+        document.querySelector('.number').style.width = '30rem'
+
+      // When guess is to high  
     } else if (guess > secretNumber) {
         if (score > 1) {
             message('📈 Too high!')
@@ -29,6 +37,8 @@ document.querySelector('.check').addEventListener('click', () => {
             textScore(0)
             message('😱 You lost the game!')
         }
+
+      // When guess is too low  
     } else if (guess < secretNumber) {
         if (score > 1) {
             message('📉 Too low!')
